894ca62db38b51a656c2fb677009bca52818316d
Shaun Walsh <153730091+Shaun-Walsh@users.noreply.github.com>
2024-05-01 21:58:08 +0100

    poc 3 nearly completed, most js intecting correctly on amsterdam.html which is the current test page. Url params are working to determine city

--- _site/js/city_focus.js
+++ _site/js/city_focus.js
@@ -1,32 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
-           
-            const currentCity = 'berlin';
+            const urlParams = new URLSearchParams(window.location.search);
+            const currentCity = urlParams.get('city');
             const dailyData = weatherData[currentCity + "_daily"].daily
             const hourlyData = weatherData[currentCity + "_hourly"].hourly
+            currentCity.innerHTML = document.getElementById("currentCity");
 
             let weather = 'Unknown'
             const weatherCode = dailyData.weather_code[0];
             if(weatherCode === 0){
-                weather = "/images/sun.png"; // or weatherImage = /images/weatherIcons/clearsky.png
+                weather = "/images/sun.png";
             } else if(weatherCode >= 1 && weatherCode <= 3){
-                weather = "/images/overcast.png"; // or weatherImage = /images/weatherIcons/overcast.png
+                weather = "/images/overcast.png";
+            } else if(weatherCode >= 51 && weatherCode <= 67){
+                weather = "/images/rain.png";
             }
 
-            
+            let weatherPlusOne = 'Unknown'
+            const weatherCodePlusOne = dailyData.weather_code[1];
+            if(weatherCodePlusOne === 0){
+                weather = "/images/sun.png";
+            } else if(weatherCodePlusOne >= 1 && weatherCodePlusOne <= 3){
+                weatherPlusOne = "/images/overcast.png";
+            } else if(weatherCodePlusOne >= 51 && weatherCodePlusOne <= 67){
+                weatherPlusOne = "/images/rain.png";
+            }
 
             const weatherCodeTodayElement = document.getElementById("weatherCodeToday");
             const weatherTodayElement = document.getElementById("weatherToday");
+            const weatherTodaySummary = document.getElementById("weatherTodaySummary");
             const weatherCodeTomorrowElement = document.getElementById("weatherCodeTomorrow");
+            const weatherPlusOneElement = document.getElementById("weatherPlusOne");
 
             weatherCodeTodayElement.innerHTML = weatherCode;
-            weatherTodayElement.innerHTML = weather;
+            weatherTodayElement.src = weather;
+            weatherTodaySummary.src = weather;
+            weatherPlusOneElement.src = weatherPlusOne;
             weatherCodeTomorrowElement.innerHTML = dailyData.weather_code[1];
 
-            const now = dayjs(); // create a new date object that represents this time
-            const currentHour = now.hour(); // 0 - 23 as a number
+            const now = dayjs(); 
+            const currentHour = now.hour();
 
-            // we need the index of the current hour in the time array
-            // we then use that index to query the termperature_2m data
             const indexOfCurrentHour = hourlyData.time.indexOf(`TodayT${currentHour}:00`);
             const indexOfCurrentHourTomorrow = hourlyData.time.indexOf(`Today+1T${currentHour}:00`);
             console.log(`The index of the current hour is : ${indexOfCurrentHour}`);
@@ -34,17 +47,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
             const timeElement = document.getElementById("timeNow");
             const tempElement = document.getElementById("tempNow");
-            const windElement = document.getElementById("windNow")
+            const windElement = document.getElementById("windNow");
 
             const tempTomorrowElement = document.getElementById("tempTomorrow");
             const dayAfterTomorrowElement = document.getElementById("dayAfterTomorrow");
 
             const dayElement = document.getElementById("day");
-            const tomorrowElement = document.getElementById("tomorrow");
+            
 
             timeElement.innerHTML = `${currentHour}:00`;
             dayElement.innerHTML = now.format("dddd");
-            tomorrowElement.innerHTML = now.add(1, 'day').format("dddd");
+            
             dayAfterTomorrowElement.innerHTML = now.add(2, 'day').format("dddd");
 
             tempElement.innerHTML = hourlyData.temperature_2m[indexOfCurrentHour];
@@ -59,4 +72,25 @@ document.addEventListener("DOMContentLoaded", () => {
             const todayMaxWind = dailyData.wind_speed_10m_max[0];
             const maxWindElement = document.getElementById("maxWind");
             maxWindElement.innerHTML = todayMaxWind;
+
+            const dayElementSummary = document.getElementById("daySummary");
+            dayElementSummary.innerHTML = now.format("dddd");
+
+            const tomorrowElement = document.getElementById("tomorrow");
+            tomorrowElement.innerHTML = now.add(1, 'day').format("dddd");
+
+            const tomorrowPlusOneElement = document.getElementById("tomorrowPlusOne");
+            tomorrowPlusOneElement.innerHTML = now.add(2, 'day').format("dddd");
+
+            const tomorrowPlusTwoElement = document.getElementById("tomorrowPlusTwo");
+            tomorrowPlusTwoElement.innerHTML = now.add(3, 'day').format("dddd");
+
+            const tomorrowPlusThreeElement = document.getElementById("tomorrowPlusThree");
+            tomorrowPlusThreeElement.innerHTML = now.add(4, 'day').format("dddd");
+
+            const tomorrowPlusFourElement = document.getElementById("tomorrowPlusFour");
+            tomorrowPlusFourElement.innerHTML = now.add(5, 'day').format("dddd");
+
+            const tomorrowPlusFiveElement = document.getElementById("tomorrowPlusFive");
+            tomorrowPlusFiveElement.innerHTML = now.add(6, 'day').format("dddd");
         });
